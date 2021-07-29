c8d8b08098c9a07c8ea30f3aa55f0bd145532ac3
Roman TIMAKOV <timakov_r@M11highway.pro>
2021-07-29 16:45:54 +0300

    not Yet

--- src/pages/script.js
+++ src/pages/script.js
@@ -1,2 +1,45 @@
-const Cards = document.querySelector('.cards');
-const Card = document.querySelector('.card');
+const listContainerElements = document.querySelector(".cards");
+const templateElement = document.querySelector(".template");
+
+const initialCards = [{
+        title: "Архыз",
+    },
+    {
+        title: "Челябинская область",
+    },
+    {
+        title: "Иваново",
+    },
+    {
+        title: "Камчатка",
+    },
+    {
+        title: "Холмогорский район",
+    },
+    {
+        title: "Байкал",
+    },
+    {
+        title: "Камчатка",
+    },
+    {
+        title: "Камчатка",
+    },
+];
+
+
+
+function renderList() {
+    const listItems = initialCards.map(composeItem);
+    listContainerElements.append(...listItems);
+}
+
+function composeItem(item) {
+    const newItem = templateElement.content.cloneNode(true);
+    const Kyk = newItem.querySelector(".kyk");
+    Kyk.textContent = item.title;
+    return newItem
+}
+
+
+renderList();
